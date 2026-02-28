976c13aff8235ce7683f6b16dd135584fccdd4f4
Sisu Koskela <246478576+Sisufys@users.noreply.github.com>
2026-02-28 13:08:48 +0200

    non blocking animations

--- animations.c
+++ animations.c
@@ -5,8 +5,7 @@ void delayFade(int n);
 void fadeall(uint8_t speed);
 void updateSnowfall(int delay, int lenghtTicks);
 void updateBreathe(int cycles, int delay);
-void updateSparkle();
-void newSparkle(int i);
+void updateBars(int delay, uint8_t rand);
 
 //upper and lower triangle animations
 uint8_t tri1[16] = {
@@ -45,27 +44,73 @@ uint8_t tri2[16] = {
     16,
     15};
 
+uint8_t bars[5][8]={{-1,26,27,28,29,30,31,0},   //upper hypotenuse
+                    {-1,1,2,3,4,5,6,-1},         //upper leg
+                    {20,21,22,23,24,25,-1,-1},  //middle line
+                    {7,8,9,10,11,12,-1,-1},     //lower leg
+                    {13,14,15,16,17,18,19,-1}}; //lower hypotenuse
+
+uint8_t barBrg[5][2]; //0 = brightness, 1 = direction
+
 uint16_t randomval;
 int count = 0;
 uint32_t lastTicks = 0;
 int n = 0;
 int animation = 0;
 uint8_t dir = 1;
+int a = 0;
 int b = 0;
 
+void updateBars(int delay, uint8_t rand){
+    for(int i = 0; i < 5; i++){
+        if(!rand && ((b - (38 * i)) % 200 == 0)){
+            barBrg[i][1] = 1;
+            barBrg[i][0] = 1;
+        }
 
+        if(!barBrg[i][1]) continue;//if the bar wont change continue the loop
 
-void delayFade(int n){ //fade for n * 4 ms
-    int i = 0;
-    while(i < n){
-        if(frame_tick){
-            fadeall(1);
-            frame_tick = 0;
-            i++;
+        if(barBrg[i][0] >= 63){
+            barBrg[i][1] = -1;
+        }
+
+        barBrg[i][0] += barBrg[i][1];
+
+        if(barBrg[i][0] <= 1){
+            barBrg[i][1] = 0;
+        }
+        for(int c = 0; c < 8; c++){
+            if(bars[i][c] >= 0){
+                setLed(bars[i][c], barBrg[i][0]);
+            }
         }
     }
+    if(rand && (b % 48 == 0)){
+        int tries = 0;
+        int passed = 0;
+        int i;
+        while((tries <= 20) && (passed == 0)){
+            i = (random() >> 11) % 5; //brute force an index where a bar is not already on
+            if(barBrg[i][1] == 0){
+                passed = 1;
+            }
+            tries++;
+        }
+        //turn the new bar on
+        barBrg[i][1] = 1;
+        barBrg[i][0] = 1;
+    }
+    b++;
+    if(b >= 1000){
+        for(int i = 0; i < 5; i++){
+            barBrg[i][0] = 0;
+            barBrg[i][1] = 0;
+        }
+        animation++;
+        b = 0;
+        delayFade(100);
+    }
 }
-
 int random(){
     randomval = randomval*2053u + 13849u; //uint16 for wraparound
     return randomval;
@@ -110,8 +155,20 @@ void updateBreathe(int cycles, int delay){
         }
     }
     if(n >= cycles){
-        animation = 0;
+        animation++;;
         n = 0;
+        b = 0;
+    }
+}
+
+void delayFade(int n){ //fade for n * 4 ms ,,,todo non blocking solution to fading between animations
+    int i = 0;
+    while(i < n){
+        if(frame_tick){
+            fadeall(1);
+            frame_tick = 0;
+            i++;
+        }
     }
 }
 
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "animations.c"
 
+uint8_t fadeSpeed[5] = {1,0,0,1,1};
 
 void main (void){
     //set the pwm engine ready
@@ -15,37 +16,36 @@ void main (void){
     int i = 1;
     int n = 0;
     int lastButtonPress = 0;
-    int nextSparkle = 0;
     while(1){
         if(frame_tick){ //fade leds if any on while keeping ye button responsive
-            if(animation != 1) fadeall(1);
+            fadeall(fadeSpeed[animation]);
             frame_tick = 0;
-            //if(sparkles && (ticks % 3 == 0)) updateSparkle();
             //if(ticks % 100 == 0) setLed((random() >> 11), 63);
+            switch(animation){
+                case(0):
+                    updateSnowfall(8, 80);
+                    break;
+                case(1):
+                    updateBreathe(3, 2);
+                    break;
+                case(2):
+                    updateBars(1, 0);
+                    break;
+                case(3):
+                    updateBars(1, 1);
+                    break;
+            }
+            if(animation >= 4){
+                animation = 0;
+            }
         }
         
-        switch(animation){
-            case(0):
-                updateSnowfall(8, 80);
 
-                break;
-            case(1):
-                updateBreathe(3, 2);
-                break;
-        }
-        
-        
-        //triangles(4,14);
-        //roundaround(4,10);
-        //doublereverseround(5,7);
-        //mirror(2, 10);
         uint8_t buttonState = (PINB >> PB6) & 1; //state is 0 or 1, 0 being button pressed
-        if(!buttonState && (ticks - lastButtonPress > 25)){ //if button is pulled low and the last button press is over 50 ms ago, debouncing
+        if(!buttonState && (ticks - lastButtonPress > 5)){ //if button is pulled low and the last button press is over 50 ms ago, debouncing
             lastButtonPress = ticks;
             setLed((random() >> 11), 63);
-            //newSparkle(nextSparkle);
-            //nextSparkle++;
-            //if(nextSparkle > 2) nextSparkle = 0;
+
         }
     }
 }
