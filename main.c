8dbb2c630832eebb97131d712ecd3bde30b3c0a6
Sisufys <246478576+Sisufys@users.noreply.github.com>
2026-03-02 14:14:31 +0200

    button functionality changed

--- main.c
+++ main.c
@@ -20,36 +20,35 @@ void main (void){
         if(frame_tick){ //fade leds if any on while keeping ye button responsive
             fadeall(fadeSpeed[animation]);
             frame_tick = 0;
-            //if(ticks % 100 == 0) setLed((random() >> 11), 63);
 
             if(animation >= 5){
                 animation = 0;
             }
-                switch(animation){
-                    case(0):
-                        updateBreathe(10, 2);
-                        break;
-                    case(1):
-                        update1dWaves(4, 10, 1, 1);
-                        break;
-                    case(2):
-                        update2dWaves(8, 10, 1);//speed 8 prtty good
-                        break;
-                    case(3):
-                        updateBars(1, 10, 0);
-                        break;
-                    case(4):
-                        updateSnowfall(8, 80);
-                        break;
-                }
+            switch(animation){
+                case(0):
+                    updateBreathe(10, 2);
+                    break;
+                case(1):
+                    update1dWaves(4, 10, 1, 1);
+                    break;
+                case(2):
+                    update2dWaves(8, 10, 1);//speed 8 prtty good
+                    break;
+                case(3):
+                    updateBars(1, 10, 0);
+                    break;
+                case(4):
+                    updateSnowfall(8, 160);
+                    break;
+            }
         }
 
 
         uint8_t buttonState = (PINB >> PB6) & 1; //state is 0 or 1, 0 being button pressed
-        if(!buttonState && (ticks - lastButtonPress > 5)){ //if button is pulled low and the last button press is over 50 ms ago, debouncing
+        if(!buttonState && (ticks - lastButtonPress > 5)){ //if button is pulled low and the last button press is over n * 4 ms ago, debouncing
             lastButtonPress = ticks;
-            setLed((random() >> 11), 63, 0);
-
+            animation++;
+            delayFade(100, 1);
         }
     }
 }
