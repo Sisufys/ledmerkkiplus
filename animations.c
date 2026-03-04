d9de6ede64f9785527f3143a744930c35560bce1
Sisu Koskela <246478576+Sisufys@users.noreply.github.com>
2026-03-04 23:27:12 +0200

    new animation and tweaks

--- animations.c
+++ animations.c
@@ -1,4 +1,5 @@
 #include "PWM-engine.c"
+
 int random();
 void delayFade(int n, int speed);
 void fadeall(uint8_t speed);
@@ -7,8 +8,7 @@ void updateBreathe(int cycles, int delay);
 void updateBars(int delay, int n, uint8_t rand);
 void update2dWaves(int speed, int n, uint8_t gammaCorrection);
 void update1dWaves(int speed, int n, int freq, uint8_t gammaCorrection);
-void lineTriWave(int speed, int freq, int gammaCorrection);
-void planeTriWave(int speed, int freq, int gammaCorrection);
+void ripple(int speed, int n, int freq, uint8_t gammaCorrection);
 
 //all leds mapped into a 2d grid
 const uint8_t coords[LEDCOUNT][2] = { //led, (x, y) grid is size (120, 200)
@@ -46,7 +46,7 @@ const uint8_t coords[LEDCOUNT][2] = { //led, (x, y) grid is size (120, 200)
     {50, 186},
 };
 
-//lookup table for sin
+//lookup table for 32 * 32sin(512/2pi *x)
 const uint8_t sineLut[512] PROGMEM = {
     32, 32, 33, 33, 34, 34, 34, 35, 35, 35, 36, 36, 37, 37, 37, 38, 38, 38, 39, 39, 40, 40, 40, 41,
     41, 41, 42, 42, 42, 43, 43, 44, 44, 44, 45, 45, 45, 46, 46, 46, 47, 47, 47, 48, 48, 48, 49, 49,
@@ -61,15 +61,50 @@ const uint8_t sineLut[512] PROGMEM = {
     38, 38, 37, 37, 37, 36, 36, 35, 35, 35, 34, 34, 34, 33, 33, 32, 32, 32, 31, 31, 30, 30, 30, 29,
     29, 29, 28, 28, 27, 27, 27, 26, 26, 26, 25, 25, 24, 24, 24, 23, 23, 23, 22, 22, 22, 21, 21, 20,
     20, 20, 19, 19, 19, 18, 18, 18, 17, 17, 17, 16, 16, 16, 15, 15, 15, 14, 14, 14, 14, 13, 13, 13,
-    12, 12, 12, 11, 11, 11, 11, 10, 10, 10, 10, 9, 9, 9, 9, 8, 8, 8, 8, 7, 7, 7, 7, 6,
-    6, 6, 6, 6, 5, 5, 5, 5, 5, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 2,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6,
-    6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11, 12, 12,
+    12, 12, 12, 11, 11, 11, 11, 10, 10, 10, 10, 9, 9, 9, 9, 8, 8, 8, 8, 7, 7, 7, 7,  6,  6,  6,  6,
+    6,  5,  5,  5,  5,  5,  4,  4,  4,  4,  4,  4, 4, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2,  2,  2,  2,  2,
+    2,  2,  2,  2,  1,  1,  1,  1,  1,  1,  1, 1, 1, 1, 1, 1, 1, 1,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5,
+    5, 5, 6, 6, 6,6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 11, 11, 12, 12,
     12, 13, 13, 13, 14, 14, 14, 14, 15, 15, 15, 16, 16, 16, 17, 17, 17, 18, 18, 18, 19, 19, 19, 20,
     20, 20, 21, 21, 22, 22, 22, 23, 23, 23, 24, 24, 24, 25, 25, 26, 26, 26, 27, 27, 27, 28, 28, 29,
     29, 29, 30, 30, 30, 31, 31, 32};
+
+//lookup table for the distance of led on index i from the point (60,100): sqrt((x-60)²+(y-100)²)
+const uint8_t dists[32] PROGMEM = {
+    100,
+    86,
+    70,
+    55,
+    39,
+    24,
+    8,
+    7,
+    23,
+    39,
+    59,
+    70,
+    86,
+    100,
+    87,
+    72,
+    60,
+    52,
+    49,
+    52,
+    61,
+    41,
+    20,
+    19,
+    41,
+    61,
+    52,
+    48,
+    51,
+    60,
+    71,
+    87,
+};
 
 //upper and lower triangle animations
 const uint8_t tri1[16] = {
@@ -108,11 +143,11 @@ const uint8_t tri2[16] = {
     16,
     15};
 
-const uint8_t bars[5][8]={{-1,26,27,28,29,30,31,0},   //upper hypotenuse
-                    {-1,1,2,3,4,5,6,-1},         //upper leg
-                    {20,21,22,23,24,25,-1,-1},  //middle line
-                    {7,8,9,10,11,12,-1,-1},     //lower leg
-                    {13,14,15,16,17,18,19,-1}}; //lower hypotenuse
+const uint8_t bars[5][8]={  {-1,26,27,28,29,30,31,0},   //upper hypotenuse
+                            {-1,1,2,3,4,5,6,-1},        //upper leg
+                            {20,21,22,23,24,25,-1,-1},  //middle line
+                            {7,8,9,10,11,12,-1,-1},     //lower leg
+                            {13,14,15,16,17,18,19,-1}}; //lower hypotenuse
 
 uint8_t barBrg[5][2]; //0 = brightness, 1 = direction
 
@@ -122,38 +157,21 @@ uint32_t lastTicks = 0;
 int n = 0;
 int animation = 0;
 uint8_t dir = 1;
-int a = 0;
 int b = 0;
+int dim = 0;
 
-void lineTriWave(int speed, int freq, int gammaCorrection){ //untested
-     //x axis is led index i, y axis is brightness
+void ripple(int speed, int n, int freq, uint8_t gammaCorrection){
     for(int i = 0; i < LEDCOUNT; i++){
-        int phase = (freq * i + speed * b) & 127;
-        int brg = (phase > 63) ? 63 - phase % 63 : phase;
+        //the following expressions should be effectively: f(x,y) = 32 + 32*sin(sqrt((x-60)²+(y-100)²)-a), where z is brightness of a led in (x,y)
+        //where the expression sqrt((x-60)²+(y-100)²) is precalculated in dists
+        int phase = (pgm_read_byte(&dists[i])*freq - speed * b) & 511;
+        int brg = pgm_read_byte(&sineLut[phase]);
         if(b <= 64) brg = brg - (64 - b);
         setLed(i, brg, gammaCorrection);
     }
     b++;
-    if(b >= n * 512 / speed){
-        dir = 1;
-        b = 0;
-        animation++;
-        delayFade(100, 1);
-    }
-}
 
-void planeTriWave(int speed, int freq, int gammaCorrection){ //untested
-    for(int i = 0; i < LEDCOUNT; i++){
-        int x = coords[i][0];
-        int y = coords[i][1];
-        int phase = (freq * (y - 1/2 * x) + speed * b) & 127;
-        int brg = (phase > 63) ? 63 - phase % 63 : phase;
-        if(b <= 64) brg = brg - (64 - b);
-        setLed(i, brg, gammaCorrection);
-    }
-    b++;
     if(b >= n * 512 / speed){
-        dir = 1;
         b = 0;
         animation++;
         delayFade(100, 1);
@@ -163,6 +181,7 @@ void planeTriWave(int speed, int freq, int gammaCorrection){ //untested
 void updateSineBreathe(int speed, int n, int freq, int gammaCorrection){
     int phase = (freq + speed * b) & 511;
     int brg = pgm_read_byte(&sineLut[phase]);
+    if(b <= 64) brg = brg - (64 - b);
     for(int i = 0; i < LEDCOUNT; i++){
         setLed(i, brg, gammaCorrection);
     }
@@ -188,7 +207,7 @@ void update1dWaves(int speed, int n, int freq, uint8_t gammaCorrection){
         dir = 1;
         b = 0;
         animation++;
-        delayFade(100, 1);
+        delayFade(128, 2);
     }
 }
 
@@ -198,7 +217,7 @@ void update2dWaves(int speed, int n, uint8_t gammaCorrection){
         int x = coords[i][0];
         int y = coords[i][1];
         //int brg = 32 + 31*sin(0.025*(y - x/2) + 0.05 * b); //runs about 1 fps
-        int phase = ((y - (2*x/3)) * 3 + speed * b) & 511;//0b 0000 0001 1111 1111, effectively the same as taking the modulus %512
+        int phase = ((3*y - x) + speed * b) & 511;//0b 0000 0001 1111 1111, effectively the same as taking the modulus %512
         int brg = pgm_read_byte(&sineLut[phase]);
         if(b <= 64) brg = brg - (64 - b);
         setLed(i, brg, gammaCorrection);
@@ -213,9 +232,9 @@ void update2dWaves(int speed, int n, uint8_t gammaCorrection){
     }
 }
 
-void updateBars(int delay, int n, uint8_t rand){
+void updateBars(int delay, int n, uint8_t rand){ //TODO delay
     for(int i = 0; i < 5; i++){
-        if(!rand && ((b - (38 * i)) % 200 == 0)){
+        if(!rand && ((b - (38 * i)) % 200 == 0)){ //if no random flag and timing
             barBrg[i][1] = 1;
             barBrg[i][0] = 1;
         }
@@ -237,7 +256,7 @@ void updateBars(int delay, int n, uint8_t rand){
             }
         }
     }
-    if(rand && (b % 48 == 0)){
+    if(rand && (b % 48 == 0)){ //if random flag is set
         int tries = 0;
         int passed = 0;
         int i;
@@ -252,8 +271,9 @@ void updateBars(int delay, int n, uint8_t rand){
         barBrg[i][1] = 1;
         barBrg[i][0] = 1;
     }
+
     b++;
-    if(b >= n * 200){
+    if(b >= n * 200 * delay){
         for(int i = 0; i < 5; i++){
             barBrg[i][0] = 0;
             barBrg[i][1] = 0;
--- main.c
+++ main.c
@@ -26,16 +26,17 @@ void main (void){
             }
             switch(animation){
                 case(0):
-                    updateBreathe(10, 2);
+                    ripple(3, 10, 4, 0);
+                    //updateBreathe(10, 2);
                     break;
                 case(1):
-                    update1dWaves(4, 10, 1, 1);
+                    update1dWaves(2, 10, 1, 1);
                     break;
                 case(2):
-                    update2dWaves(8, 10, 1);//speed 8 prtty good
+                    update2dWaves(2, 10, 1);
                     break;
                 case(3):
-                    updateBars(1, 10, 0);
+                    updateBars(2, 10, 0);
                     break;
                 case(4):
                     updateSnowfall(8, 160);
@@ -45,10 +46,11 @@ void main (void){
 
 
         uint8_t buttonState = (PINB >> PB6) & 1; //state is 0 or 1, 0 being button pressed
-        if(!buttonState && (ticks - lastButtonPress > 5)){ //if button is pulled low and the last button press is over n * 4 ms ago, debouncing
+        if(!buttonState && (ticks - lastButtonPress > 5)){ //if button is pressed and the last button press is over n * 4 ms ago, debouncing
             lastButtonPress = ticks;
             animation++;
             delayFade(100, 1);
+            b = 0;
         }
     }
 }
